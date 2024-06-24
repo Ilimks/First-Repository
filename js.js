48b66c9c8afeaa26d703635ca4985f60aebca026
alibek <alibek1024@mail.ru>
2024-06-24 17:10:24 +0600

    alibek commit

---
+++ js.js
@@ -0,0 +1,19 @@
+    //  var swiper = new Swiper(".mySwiper", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 30,
+    //     loop:true,
+    //     autoplay:true,
+        
+        
+                
+    //   });
+
+
+      let popup = document.querySelector('.popup')
+
+      let popupToggle = ()=>{
+        popup.classList.toggle('active')
+      }
+
+
+   
